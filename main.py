d88b946d3873eee5467d3ee96e5a9ff1f78be40f
JungwookLee <jungwooklee59@naver.com>
2023-07-20 16:45:14 +0900

    제곱함수있는버전커밋

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 20 14:20:09 2023
-
-@author: jungw
-"""
-
 ##함수 선언부
 def add_func(n1, n2):
     return n1 + n2
 
 def sub_func(n1, n2):
     return n1 - n2
+
+def sqr_func(n1):
+    return n1 ** 2
 
 ##전역 변수부
 num1, num2, res = 100, 200, 0
@@ -21,3 +17,6 @@ print(num1, "+", num2, '=', res)
 
 res = sub_func(num1, num2)
 print(num1, '-', num2, '=', res)
+
+res = sqr_func(num1)
+print(num1, '^2', '=', res)
